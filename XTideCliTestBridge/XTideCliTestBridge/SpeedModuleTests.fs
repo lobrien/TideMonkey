bf77e5e5ea8fb03048afab5c330d12407976cf6b
lobrien <lobrien@knowing.net>
2016-01-22 16:13:02 -1000

    Speed module unit tests

---
+++ XTideCliTestBridge/XTideCliTestBridge/SpeedModuleTests.fs
@@ -0,0 +1,17 @@
+﻿namespace XTideCliTestBridge
+
+open System
+open NUnit.Framework
+open TideMonkey
+
+[<TestFixture>]
+type SpeedModuleTests() =
+    
+   [<Test>]
+   member x.CanConvertDPHtoRPS() = 
+      let oncePerHour = 360.0<Degrees / Hours>
+      let radians = Speed.Convert oncePerHour
+      let radiansPerHour = float radians
+      let radiansPerMinute = radiansPerHour / 60.
+      let radiansPerSecond = radiansPerMinute / 60.
+      Assert.AreEqual(radiansPerSecond, float radians, 0.01)
